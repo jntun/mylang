da4826d4d9680194fc6a94539f37a177bc1e5a4c
jntun <justin@jntun.com>
2021-07-20 11:08:43 -0700

    Builtin: Add 'makeBuiltinFunc' method for cleaner builtin function addition

--- lang/builtin.go
+++ lang/builtin.go
@@ -27,19 +27,22 @@ func globals() []Statement {
 
 	globals = append(globals, VariableStatement{Identifier: Token{Lexeme: "pi", Type: Identifier, Line: 0}, Expr: Literal{Token{Lexeme: "3.1415926535", Type: Number, Line: 0}}})
 
-	args := append(make([]Token, 0), Token{"v", Identifier, 0})
-	globals = append(globals, FunctionDeclarationStatement{
-		Identifier: Token{
-			Lexeme: "len",
-			Type:   Identifier,
-			Line:   0,
-		},
-		args: &args,
-		block: append(make([]Statement, 0), ReturnStatement{
-			Expression: Len{},
-			val:        nil,
-		}),
-	})
+	globals = append(globals, makeBuiltinFunc("len", []string{"v"}, []Statement{
+		ReturnStatement{Len{}, nil},
+	}))
 
 	return globals
+}
+
+func makeBuiltinFunc(identifier string, args []string, block []Statement) FunctionDeclarationStatement {
+	tokenArgs := make([]Token, len(args))
+	for i, arg := range args {
+		tokenArgs[i] = Token{arg, Identifier, 0}
+	}
+
+	return FunctionDeclarationStatement{
+		Identifier: Token{identifier, Identifier, 0},
+		args:       &tokenArgs,
+		block:      block,
+	}
 }
